ef0361f7a7e333a1de838217caad56d024c94daa
kip <cyruskiprop254@gmail.com>
2020-04-13 13:51:38 +0300

    fix rubocop errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_04_09_200626) do
-
   create_table 'users', force: :cascade do |t|
     t.string 'username'
     t.string 'email'
@@ -19,5 +18,4 @@ ActiveRecord::Schema.define(version: 2020_04_09_200626) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
-
 end
