0e210c3c72caf24330cb630a986e1949344f5fa1
kip <cyruskiprop254@gmail.com>
2020-04-14 21:34:33 +0300

    add edit, index, new, and show actions to the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
-    @user = User.new(username: 'Cyrus Kiprop', email: 'cyruskiprop@gmail.com', password: 'dfdslfds34')
+    @user = User.new(username: 'Cyrus Kiprop', email: 'cyruskiprop@gmail.com')
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
@@ -9,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_users_path
+      redirect_to users_path
     else
       render :new
     end
@@ -19,14 +27,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
   def update
     @user = User.find(params[:id])
-    @user.update
+    @user.update(user_params)
+    redirect_to users_path
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :username, :password)
+    params.require(:user).permit(:email, :username)
   end
 end
